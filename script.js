b4958c3b028f7ab1175dafcd34199169037bd2e9
divyesh <kdivyesh.cs@gmail.com>
2022-11-06 19:13:03 +0400

    in process

---
+++ script.js
@@ -0,0 +1,67 @@
+const gridContainer = document.querySelector("#gridContainer")
+
+// game board display
+const gameBoard = (() => {
+    const gameBoardArr = [" ", " ", " ", 
+                        " ", " ", " ", 
+                        " ", " ", " ",];
+
+
+    const showGameBoard = (boardVals = gameBoardArr) => {
+        gridContainer.innerHTML = ""
+        for(i=0; i<boardVals.length; i++) {
+            div = document.createElement("div") 
+            div.classList.add("gridItem")           
+            div.setAttribute('id', `${i}`)
+            div.innerHTML = boardVals[i];
+            gridContainer.appendChild(div);
+        }
+        console.log("ran function showgameboard")
+        return true;
+    }
+
+
+    //edit on click    
+    function onClick(){
+        let gridItems = document.querySelectorAll(".gridItem")
+        gridItems.forEach(item=> {
+            item.addEventListener("click", function(){
+                console.log("working")
+                gameBoardArr.splice(parseInt(item.getAttribute('id')), 1, player1.marker)
+                showGameBoard()
+                console.log("why does it stop? ehhhhhhhhh")
+            })
+        });
+
+    }
+    
+
+    //check if won or tie
+    function checkEnd(boardVals = gameBoardArr){
+        //todo
+    }
+
+    return {
+        showGameBoard,
+        onClick,
+    };
+
+})();
+gameBoard.showGameBoard();
+gameBoard.onClick();
+
+
+
+const boardControl = ((board = gameBoard.showGameBoard()) => {
+    //add marker positions based on input based on input
+    //edit on click
+});
+
+
+//players
+const players = (name, marker) => {
+    return { name, marker};
+}
+
+const player1 = players("player1", "X");
+const player2 = players("player2", "O")
